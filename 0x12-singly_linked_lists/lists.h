935d3620b8df0604dd56d10eb38225fcab797774
Muhammedessam11 <titiotitio@example.com>
2023-10-19 09:58:46 -0700

    C - SINGLY LINKED LISTS

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LINKED
+#define LINKED
 
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 /**
@@ -18,3 +18,10 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+#endif /*LINKED*/
